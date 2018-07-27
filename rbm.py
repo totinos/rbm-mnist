7be45206561b9595a616f5d802de1d4359565083
Samuel Brown <sbrow109@vols.utk.edu>
2018-07-27 14:40:56 -0400

    Removed rbm.py and rbm2.py, moved rbm3.py to rbm.py.

--- rbm.py
+++ rbm.py
@@ -1,152 +1,241 @@
+#!/usr/bin/python
+
+###########################################################################
+#                                                                         #
+#  FILENAME    -- rbm3.py                                                 #
+#                                                                         #
+#  AUTHOR      -- Sam Brown                                               #
+#                                                                         #
+#  DESCRIPTION -- This file implements functions that allow the user to   #
+#                 create and train a Restricted Boltzmann Machine using   #
+#                 any dataset.                                            #
+#                                                                         #
+###########################################################################
+
+
 import numpy as np
 import os
 import sys
 import mnist
-
 from time import time
+import matplotlib.pyplot as plt
 
-def rbm(dataset, num_hidden, learning_rate, max_epochs, batch_size):
-    """Train a RBM where the input is binary or real over interval [0,1].
 
-    The training algorithm splits the dataset into minibatches and uses
-    momentum when updating the weight values, as Hinton does in his MATLAB
-    implementation.
 
-    Args:
-        dataset - A numpy array of dim. [num_examples] x [num_inputs].
-        num_hidden - The number of hidden neurons in the RBM implementation.
-        learning_rate - Used when updating weights (ref. Geoff Hinton).
-        max_epochs - The number of epochs to train the RBM network.
-        batch_size - The number of training examples per batch.
+class RBM:
 
-    Returns:
-        weights - A numpy array of dim. [num_visible] x [num_hidden].
-        v_bias - A numpy array of dim. [num_visible] x [1].
-        h_bias - A numpy array of dim. [num_hidden] x [1].
-    """
 
-    num_examples = dataset.shape[0]
-    num_visible = dataset.shape[1]
 
-    start_time = time()
+    def __init__(self, num_visible, num_hidden, act_func='logistic'):
+        """Initializes an RBM with the given dimensions.
 
-    num_batches = num_examples // batch_size
+        Args:
+            dataset - A numpy array of dim. [num_examples] x [num_inputs].
+            num_hidden - The number of hidden neurons in the RBM implementation.
+            act_func - The type of activation function the RBM should use.
+        """
 
-    # Initialize the weight matrices
-    weights = 0.1 * np.random.randn(num_visible, num_hidden)
-    v_bias = np.zeros((1, num_visible))
-    h_bias = -4.0 * np.ones((1, num_hidden))
-    #h_bias = np.zeros((1, num_hidden))
+        print('Initializing network...  ', end='')
+        sys.stdout.flush()
 
-    # Initialize the weight incremental update matrices
-    w_inc = np.zeros((num_visible, num_hidden))
-    v_inc = np.zeros((1, num_visible))
-    h_inc = np.zeros((1, num_hidden))
+        self.num_visible = num_visible
+        self.num_hidden = num_hidden
+        
+        #self.reconstructed = np.zeros((self.num_examples, self.num_visible))
 
-    for epoch in range(max_epochs):
-        error = 0
-        for batch in range(num_batches):
+        self.weights = 0.1 * np.random.randn(num_visible, num_hidden)
+        self.v_bias = np.zeros((1, num_visible))
+        self.h_bias = -4.0 * np.ones((1, num_hidden))
 
-            ####### POSITIVE CONTRASTIVE DIVERGENCE PHASE ######
-            
-            # Get next batch of data
-            start = int(batch * batch_size)
-            end = int((batch+1) * batch_size)
-            data = dataset[start:end]
-            
-            # In this matrix, m[i,j] is prob h[j] = 1 given example v[i]
-            # Dim. [num_examples] x [num_hidden]
-            pos_hid_probs = logistic(data, weights, h_bias)
+        self.w_inc = np.zeros((num_visible, num_hidden))
+        self.v_inc = np.zeros((1, num_visible))
+        self.h_inc = np.zeros((1, num_hidden))
 
-            # Sample the states of the hidden units based on pos_hid_probs
-            pos_hid_states = pos_hid_probs > np.random.rand(batch_size, num_hidden)
+        self.act_func = self.lookup if act_func == 'lookup' else self.logistic
 
-            # Positive phase products
-            pos_prods = np.dot(data.T, pos_hid_probs)
+        print('Done!')
+        return
 
-            # Activation values needed to update biases
-            pos_hid_act = np.sum(pos_hid_probs, axis=0)
-            pos_vis_act = np.sum(data, axis=0)
 
-            ###### NEGATIVE CONTRASTIVE DIVERGENCE PHASE ######
 
-            # Reconstruct the data by sampling the vsible states from hidden states
-            neg_data = logistic(pos_hid_states, weights.T, v_bias)
+    def train(self, dataset, learning_rate=0.1, max_epochs=50, batch_size=100):
+        """Train a RBM where the input is binary or real over interval [0,1].
 
-            # Sample hidden states from visible states
-            neg_hid_probs = logistic(neg_data, weights, h_bias)
+        The training algorithm splits the dataset into minibatches and uses
+        momentum when updating the weight values, as Hinton does in his MATLAB
+        implementation.
 
-            # Negative phase products
-            neg_prods = np.dot(neg_data.T, neg_hid_probs)
+        Args:
+            learning_rate - Used when updating weights (ref. Geoff Hinton).
+            max_epochs - The number of epochs to train the RBM network.
+            batch_size - The number of training examples per batch.
+        """
 
-            # Activation values needed to update biases
-            neg_hid_act = np.sum(neg_hid_probs, axis=0)
-            neg_vis_act = np.sum(neg_data, axis=0)
+        num_examples = dataset.shape[0]
+        num_batches = num_examples // batch_size
 
-            ###### UPDATE WEIGHT VALUES ######
+        self.reconstructed = None
+        self.reconstructed = np.zeros((num_batches, self.num_visible))
 
-            # Set momentum as per Hinton's methods
-            m = 0.5 if epoch > 5 else 0.9
+        # Train the RBM over given # of epochs using Contrastive Divergence
+        start_time = time()
+        for epoch in range(max_epochs):
+            error = 0
+            momentum = 0
+            for batch in range(num_batches):
+                start = int(batch * batch_size)
+                end = int((batch+1) * batch_size)
+                data = dataset[start:end]
 
-            # Update the weights
-            x = learning_rate/batch_size
-            w_inc = (w_inc * m) + x * (pos_prods - neg_prods)
-            v_inc = (v_inc * m) + x * (pos_vis_act - neg_vis_act)
-            h_inc = (h_inc * m) + x * (pos_hid_act - neg_hid_act)
+                ############# POSITIVE #############
+                pos_hid_probs = self.act_func(data, self.weights, self.h_bias)
+                rand_states = np.random.rand(batch_size, self.num_hidden)
+                pos_hid_states = pos_hid_probs > rand_states
+                pos_prods = np.dot(data.T, pos_hid_probs)
+                pos_hid_act = np.sum(pos_hid_probs, axis=0)
+                pos_vis_act = np.sum(data, axis=0)
 
-            weights += w_inc
-            v_bias += v_inc
-            h_bias += h_inc
+                ############# NEGATIVE #############
+                neg_data = self.act_func(pos_hid_states, self.weights.T, self.v_bias)
+                neg_hid_probs = self.act_func(neg_data, self.weights, self.h_bias)
+                neg_prods = np.dot(neg_data.T, neg_hid_probs)
+                neg_hid_act = np.sum(neg_hid_probs, axis=0)
+                neg_vis_act = np.sum(neg_data, axis=0)
 
-            error += np.sum((data - neg_data) ** 2)
+                #############  UPDATE  #############
+                m = 0.5 if epoch > 5 else 0.9
+                x = learning_rate/batch_size
+                self.w_inc = (self.w_inc * m) + x*(pos_prods - neg_prods)
+                self.v_inc = (self.v_inc * m) + x*(pos_vis_act - neg_vis_act)
+                self.h_inc = (self.h_inc * m) + x*(pos_hid_act - neg_hid_act)
+                self.weights += self.w_inc
+                self.v_bias += self.v_inc
+                self.h_bias += self.h_inc
+
+                error += np.sum((data - neg_data) ** 2)
+
+                ############ RECONSTRUCT ###########
+                self.reconstructed[batch] = neg_data[0]
+
+            # TODO ---> ADD programmable sampling frequency
+            # TODO ---> ADD a nice progress bar for convenience??
+            #time_elapsed = time() - start_time
+            if max_epochs < 10 or epoch % (max_epochs // 10) == 0:
+                print('Epoch %4d -> Reconstruction error: %0.2f.' % (epoch, error))
+                if not os.path.exists('reconstructed'):
+                    os.makedirs('reconstructed')
+                filename = 'reconstructed/{:04}.png'.format(epoch)
+                self.save_reconstructed_images(filename, 28, 28)
 
         time_elapsed = time() - start_time
-        print('Epoch %4d completed. Reconstruction error is %0.2f. Sim time (s): %0.2f' % (epoch+1, error, time_elapsed))
+        print('Training completed.')
+        print('Time elapsed (s): %0.2f' % time_elapsed)
+        return
 
-    print('Training completed.')
-    return weights, v_bias, h_bias
 
 
-def logistic(x, w, b):
-    """A sigmoid relationship for RBM activation function.
+    def logistic(self, data, weights, biases):
+        """Logistic activation function"""
+        state_weight_prods = np.dot(data, weights)
+        # replicated_bias = np.tile(biases, (data.shape[0], 1))
+        return 1.0 / (1 + np.exp(- state_weight_prods - biases))
 
-    Args:
-        x - A numpy array of states.
-        w - A numpy array of weights.
-        b - A numpy array of biases.
+    def lookup(self, data, weights, biases):
+        """Stochastic neuron activation function lookup table."""
+        return
 
-    Returns:
-        A numpy array after the logistic function is applied.
-    """
 
-    xw = np.dot(x, w)
-    # replicated_b = np.tile(b, (x.shape[0], 1))
-    return 1.0 / (1 + np.exp(- xw - b))
 
-def lookup(x, w, b):
-    """A stochastic neuron activation function model.
 
-    Args:
-        x - A numpy array of states.
-        w - A numpy array of weights.
-        b - A numpy array of biases.
-    """
-    print('LOOKUP HERE.')
+    def save_reconstructed_images(self, filename, rows, cols):
+        """Saves an image representing the reconstructed data to disk.
+
+        Args:
+            filename - A location to save the image file.
+        """
+        # print(filename)
+        num_images = self.reconstructed.shape[0]
+        images = np.zeros((rows, cols*num_images))
+        for i in range(num_images):
+            start = int(i * cols)
+            end = int((i+1) * cols)
+            images[0:rows, start:end] = self.reconstructed[i].reshape((rows, cols))
+
+        plt.imshow(images, cmap='gray', aspect='equal', interpolation='none')
+        #plt.show()
+        plt.savefig(filename)
+        return
+
+
+
+    def reconstruct_image(self, input_vector):
+        """Reconstructs the network's impression of the input image.
+
+        The hidden states are sampled after the inputs are introduced, and
+        then the visible states are sampled based on the propagation of the
+        hidden states backward through the network.
+
+        Args:
+            input_vector - A numpy array of dimension [num_inputs].
+        """
+
+        pos_hid_probs = self.act_func(input_vector, self.weights, self.h_bias)
+        pos_hid_states = pos_hid_probs > np.random.rand(1, self.num_hidden)
+        neg_data = self.act_func(pos_hid_states, self.weights.T, self.v_bias)
+        return neg_data
+
+
+
+    def save_weights(self, outfile='', title=''):
+        """Save image with pictoral representation of weight distributions.
+
+        Args:
+            outfile - The filename for the image to be saved.
+            title - The title for the image to be saved.
+        """
+
+        row = self.num_visible
+        col = self.num_hidden
+        # plt.imshow(self.weights, cmap='gray', aspect='auto', interpolation='none')
+        # plt.show()
+
+        # plt.imshow(self.reconstruct_image(self.dataset[0]).reshape((28,28)), cmap='gray', aspect='equal', interpolation='none')
+        # plt.show()
+
+        # plt.imshow(self.dataset[0].reshape((28,28)), cmap='gray', aspect='equal', interpolation='none')
+        # plt.show()
+        return
+    
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 6:
+    if len(sys.argv) == 7:
         num_examples = int(sys.argv[1])
         num_hidden = int(sys.argv[2])
         max_epochs = int(sys.argv[3])
         learning_rate = float(sys.argv[4])
         batch_size = int(sys.argv[5])
+        act_func = str(sys.argv[6])
 
         images, labels = mnist.load_images(num_examples, True)
-        w, a, b = rbm(images, num_hidden, learning_rate, max_epochs, batch_size)
+
+
+        num_visible = images.shape[1]
+        #num_examples = images.shape[0]
+
+
+        rbm = RBM(num_visible, num_hidden, act_func)
+        rbm.train(images, learning_rate, max_epochs, batch_size)
+        rbm.save_weights("", "")
+        rbm.save_reconstructed_images("", 28, 28)
+
     elif len(sys.argv) == 1:
         print('Training RBM on 1000 examples over 50 epochs with learning rate of 0.1 and 100 hidden neurons.')
         images, labels = mnist.load_images(1000, True)
-        w, a, b = rbm(images, 100, 0.1, 50, batch_size=100)
+        num_visible = images.shape[1]
+        rbm = RBM(num_visible, 100, 'logistic')
+        rbm.train(images, 0.1, 50, 100)
+
     else:
-        print('Usage: python rbm.py [num_examples] [num_hidden] [max_epochs] [learning_rate] [batch_size]')
+        print('Usage: python rbm.py [num_examples] [num_hidden] [max_epochs] [learning_rate] [batch_size] [act_func]')
         exit(1)
